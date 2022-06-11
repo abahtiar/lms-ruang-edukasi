9cfe655777e345331dcbda3d1f434f21d7b503dc
Ari Bahtiar <kewcoder@gmail.com>
2022-06-11 11:18:13 +0700

    fixed layout cover

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -1,6 +1,6 @@
 <template>
         <div class="w-full min-h-screen d-flex  bg-gray-100 p-4">
-               <div class="flex flex-wrap items-center w-1/2 m-auto bg-white rounded-xl relative">
+               <div class="flex flex-wrap items-center w-full lg:w-1/2 m-auto bg-white rounded-xl relative">
                         <Header></Header>
                         <NuxtPage class="w-full pt-20 p-10" />
                </div>
